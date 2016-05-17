0009768b61cbca3dd1ff82f24f551a345b3ef608
YIMINGXU <100650920@qq.com>
2016-05-17 17:30:07 +0800

    rocketMQ 使用PB对象序列化

---
+++ src/main/java/rocketmq/object/ConsumerObject.java
@@ -0,0 +1,47 @@
+package rocketmq.object;
+
+import base.protocolbuffer.TestPeople;
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.List;
+
+public class ConsumerObject {
+	public static void main(String[] args){
+		DefaultMQPushConsumer consumer = 
+				new DefaultMQPushConsumer("PushConsumer");
+		consumer.setNamesrvAddr("xym01:9876");
+		try {
+			//订阅PushTopic下Tag为push的消息
+			consumer.subscribe("PushTopic", "push");
+			//程序第一次启动从消息队列头取数据
+			consumer.setConsumeFromWhere(
+					ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+			consumer.registerMessageListener(
+				new MessageListenerConcurrently() {
+					public ConsumeConcurrentlyStatus consumeMessage(
+							List<MessageExt> list,
+							ConsumeConcurrentlyContext Context) {
+						Message msg = list.get(0);
+						System.out.println(msg.toString());
+						try {
+							System.out.println(TestPeople.People.parseFrom(msg.getBody()).getName());
+						} catch (InvalidProtocolBufferException e) {
+							e.printStackTrace();
+						}
+						return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+					}
+				}
+			);
+			consumer.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/rocketmq/object/ProducerObject.java
@@ -0,0 +1,57 @@
+package rocketmq.object;
+
+import base.protocolbuffer.TestPeople;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+
+public class ProducerObject {
+	public static void main(String[] args){
+		DefaultMQProducer producer = new DefaultMQProducer("Producer");
+		producer.setNamesrvAddr("192.168.80.101:9876");
+		try {
+			producer.start();
+			TestPeople.People.Builder peBuilder = TestPeople.People.newBuilder().
+					//设置各种属性
+							setEmail("100650920").
+							setName("xym").
+							setId(10).
+					//添加一个关联的子类
+							addPhone(TestPeople.People.PhoneNumber.newBuilder().setNumber("1860").setType(1));
+			TestPeople.People people = peBuilder.build();
+			byte[] buf = people.toByteArray();
+
+			Message msg = new Message("PushTopic", 
+					"push", 
+					"1",
+					buf);
+			
+			SendResult result = producer.send(msg);
+			System.out.println("id:" + result.getMsgId() +
+					" result:" + result.getSendStatus());
+
+			peBuilder = TestPeople.People.newBuilder().
+					//设置各种属性
+							setEmail("100650920").
+							setName("xym2").
+							setId(10).
+					//添加一个关联的子类
+							addPhone(TestPeople.People.PhoneNumber.newBuilder().setNumber("1860").setType(1));
+			TestPeople.People people2 = peBuilder.build();
+			byte[] buf2 = people2.toByteArray();
+			msg = new Message("PushTopic", 
+					"push", 
+					"2",
+					buf2);
+			
+			result = producer.send(msg);
+			System.out.println("id:" + result.getMsgId() +
+					" result:" + result.getSendStatus());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			producer.shutdown();
+		}
+	}
+}
