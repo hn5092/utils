b36071985945b804f0c40d62a79cb3e70986aeb7
YIMINGXU <100650920@qq.com>
2016-05-23 23:57:36 +0800

    kmP算法

---
+++ src/main/java/datastructure/BruteForce/Test.java
@@ -0,0 +1,13 @@
+package datastructure.BruteForce;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s1 = "abacabcdaa";
+		String s2 = "abbbb";
+		
+		System.out.println(BruteForce.bruteForce(s1, s2));
+	}
+
+}
